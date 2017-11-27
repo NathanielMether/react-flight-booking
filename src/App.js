e0a543380aa03ce4d5f306cd07c221998d989fe7
NathanielMether <nmet3@student.monash.edu>
2017-11-27 11:22:17 +1100

    added button component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import Button from './components/Button'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Helo</h1>
+        <Button title='Find Flights' primary />
+        <Button title='Sign up'/>
+        <Button title='Enter Competition' magic/>
       </div>
     );
   }
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,21 @@
 import React from 'react'
 
-function Button() {
+function Button(props) {
+  let className = 'btn'
 
+  if (props.primary) {
+    className += ' btn-primary'
+  }
+
+  if (props.magic) {
+    className += ' btn-rainbow'
+  }
+
+  return (
+    <button className={ className }>
+      { props.title }
+    </button>
+  )
 }
 
 export default Button
